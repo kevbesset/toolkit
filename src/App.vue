c1ab48cd1d89694b94ad5a120d220cb2614838b0
kevbesset <kevbesset@gmail.com>
2021-02-04 19:12:31 +0100

    remove demo from vite

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <h1>App.vue</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
