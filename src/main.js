8b1d37037a85b003ccf3cb2eb9dc41e91e764f47
kevbesset <kevbesset@gmail.com>
2021-02-05 11:53:47 +0100

    set up scss base

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
 
+import "normalize.css";
+import "./scss/index.scss";
+
 createApp(App).use(router).mount("#app");
