f45343c9b4da520d4ee16571544e0d1a3e7919f8
kevbesset <kevbesset@gmail.com>
2021-02-06 22:40:37 +0100

    update router history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import RouteName from "../config/RouteName";
 
 import Home from "../views/Home.vue";
@@ -14,7 +14,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
